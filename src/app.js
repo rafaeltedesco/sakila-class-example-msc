5a56843effb75a81b84fe33924dcf36acb850865
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2023-01-12 14:07:35 -0300

    refactor: remove unused filmModel

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-const { actorModel, filmModel, customerModel } = require('./models');
+const { actorModel, customerModel } = require('./models');
 const { filmService } = require('./services');
 
 
